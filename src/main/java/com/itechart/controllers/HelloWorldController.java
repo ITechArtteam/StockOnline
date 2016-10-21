ba73509501d6d12e36247db48ea5ac9db4e7389a
orelDmitriy <oreldmitriy1991@gmail.com>
2016-10-22 00:29:01 +0300

    fixed maven errors have test errors

--- src/main/java/com/itechart/controllers/HelloWorldController.java
+++ src/main/java/com/itechart/controllers/HelloWorldController.java
@@ -1,11 +1,9 @@
 package com.itechart.controllers;
 
-import com.itechart.domain.User;
 import com.itechart.services.SecurityService;
 import com.itechart.services.UserService;
 import com.itechart.validators.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,11 +21,9 @@ public class HelloWorldController {
     private UserValidator userValidator;
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public BindingResult hello(BindingResult bindingResult) {
-        User user = new User();
-        user.setFirstName("di");
-        userValidator.validate(user, bindingResult);
-        return bindingResult;
+    public String hello() {
+
+        return "ok";
     }
 
 }
