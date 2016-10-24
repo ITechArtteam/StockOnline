5fb83d01164f2eec9a7e0d6c7816682ba578375f
orelDmitriy <oreldmitriy1991@gmail.com>
2016-10-24 17:50:43 +0300

    добавил loombok и почистил модели

--- src/main/java/com/itechart/model/Role.java
+++ src/main/java/com/itechart/model/Role.java
@@ -1,11 +1,14 @@
 package com.itechart.model;
 
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.Set;
 
 @Entity
 @Table(name = "roles")
+@Data
 public class Role {
 
     @Id
@@ -18,39 +21,4 @@ public class Role {
     @ManyToMany(mappedBy = "roles")
     private Set<User> users;
 
-    public Role() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Set<User> getUsers() {
-        return users;
-    }
-
-    public void setUsers(Set<User> users) {
-        this.users = users;
-    }
-
-    @Override
-    public String toString() {
-        return "Role{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", users=" + users +
-                '}';
-    }
 }
