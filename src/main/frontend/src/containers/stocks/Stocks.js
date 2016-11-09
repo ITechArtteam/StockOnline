dd3afb81583cdb0cb77e0efc31e184bfcad3cfa7
SerhGitHub <serhiorsv@gmail.com>
2016-11-09 20:24:44 +0300

    edit Stock.js

--- src/main/frontend/src/containers/stocks/Stocks.js
+++ src/main/frontend/src/containers/stocks/Stocks.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React from 'react';
+import {stockListActionCreator} from "./index";
+import {connect} from 'react-redux';
 
 class Stocks extends React.Component {
-    render(){
-        return (
-            <div>Stocks</div>
+    componentWillMount() {
+        this.props.getStockList(1, 5);
+    }
 
-        )
+    render() {
+        // console.log(this.props.stockList);
+        var stockListElements = this.props.stockList.map((item, index)=> {
+                return <p key={index}>{item.name}</p>;
+    });
+        return (
+            <div>Stock
+        {stockListElements}
+    </div>
+    )
     }
 }
 
-export default Stocks;
+
+const mapStateToProps = (state) => {
+    // console.log(state);
+    return {
+        stockList: state.stockListReducer.stockList
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+            getStockList: (pageNumber, recordsCount) => {
+            dispatch(stockListActionCreator.getStockList(pageNumber, recordsCount))
+}
+}
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Stocks);
