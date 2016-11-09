f9d3615772e91f744b0cd693aaf08c69e2e6f205
SerhGitHub <serhiorsv@gmail.com>
2016-11-09 20:30:33 +0300

    edit reducer\ index.js

--- src/main/frontend/src/reducers/index.js
+++ src/main/frontend/src/reducers/index.js
@@ -2,10 +2,11 @@ import {combineReducers} from 'redux'
 import {reducer as FormReducer} from 'redux-form'
 import {client} from "../containers/editClient";
 import {clientListReducer} from '../containers/systemClients/index';
+import {clientListReducer} from '../containers/stocks/index';
 
 const rootReducer = combineReducers({
     form: FormReducer,
-    client, clientListReducer
+    client, clientListReducer, stockListReducer
 });
 
 export default rootReducer
