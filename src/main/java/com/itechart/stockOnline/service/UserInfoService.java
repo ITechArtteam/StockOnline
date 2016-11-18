82f6917edfc1cc935cda2e369279abc3f3e449d0
speane <avtolubitel225@gmail.com>
2016-11-18 13:07:31 +0300

    Fixed `userinfo` service.

--- src/main/java/com/itechart/stockOnline/service/UserInfoService.java
+++ src/main/java/com/itechart/stockOnline/service/UserInfoService.java
@@ -1,9 +1,11 @@
 package com.itechart.stockOnline.service;
 
-import com.itechart.stockOnline.dao.UserDao;
+import com.itechart.stockOnline.model.Role;
+import com.itechart.stockOnline.model.User;
 import com.itechart.stockOnline.model.dto.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -12,11 +14,17 @@ import java.util.Set;
 public class UserInfoService {
 
     @Autowired
-    private UserDao userDao;
+    private UserService userService;
 
+    @Transactional(readOnly = true)
     public UserInfo getUserInfo(String username) {
-        Set<String> roles = new HashSet<>();
-        roles.add("ROLE_USER");
-        return new UserInfo("user", roles);
+        User user = userService.findByName(username);
+
+        Set<String> userRolesStrings = new HashSet<>();
+        for (Role role : user.getRoles()) {
+            userRolesStrings.add(role.getName());
+        }
+
+        return new UserInfo(user.getName(), userRolesStrings);
     }
 }
