2ad422032f521148d50f51313a8499e0f61a7f37
speane <avtolubitel225@gmail.com>
2016-11-20 21:01:08 +0300

    Removed using of redux-form library for waybill registration form

--- src/main/frontend/src/containers/WaybillRegistration.js
+++ src/main/frontend/src/containers/WaybillRegistration.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table'
 import { connect } from 'react-redux'
-import { reduxForm, Field } from 'redux-form'
 import * as Actions from '../actions'
 import Select from 'react-select';
 
@@ -11,85 +10,92 @@ import Calendar from 'react-input-calendar'
 require('react-input-calendar/style/index.css');
 
 class WaybillRegistration extends React.Component {
-    renderField = ({ input, label, type, meta: { touched, error } }) => (
-        <fieldset className={`form-group ${touched && error ? 'has-error' : ''}`}>
-            <label className="control-label">{label}</label>
-            <div>
-                <input {...input} placeholder={label} className="form-control" type={type} />
-                {touched && error && <div className="help-block">{error}</div>}
-            </div>
-        </fieldset>
-    );
-
-    renderTextArea = ({ input, label, type, meta: { touched, error } }) => (
-        <fieldset className={`form-group ${touched && error ? 'has-error' : ''}`}>
-            <label className="control-label">{label}</label>
-            <div>
-                <textarea {...input} placeholder={label} className="form-control" type={type} />
-                {touched && error && <div className="help-block">{error}</div>}
-            </div>
-        </fieldset>
-    );
 
     render() {
         return (
             <div className="col-md-6 col-md-offset-3">
                 <h3 className="text-center">Регистрация накладной</h3>
                 <form>
-                    <Field name="waybill-number"
-                           component={this.renderField}
-                           className="form-control"
-                           type="text"
-                           label="Номер накладной" />
-                    <label className="control-label">Дата регистрации</label>
-                    <Calendar format='DD/MM/YYYY' />
-                    <Field name="sender"
-                           component={this.renderField}
-                           className="form-control"
-                           type="text"
-                           label="Отправитель" />
-                    <Field name="carrier"
-                           component={this.renderField}
-                           className="form-control"
-                           type="text"
-                           label="Перевозчик" />
-                    <Field name="number"
-                           component={this.renderField}
-                           className="form-control"
-                           type="text"
-                           label="Номера" />
-                    <Field name="driver"
-                           component={this.renderField}
-                           className="form-control"
-                           type="text"
-                           label="Водитель" />
-                    <Field name="description"
-                           component={this.renderTextArea}
-                           className="form-control"
-                           type="ывавыа"
-                           label="Дополнительное описание товарной партии" />
-                    <Field name="goods-total-sum"
-                           component={this.renderField}
-                           className="form-control"
-                           type="text"
-                           label="Сумма товаров по накладной" />
-                    <Field name="goods-amount"
-                           component={this.renderField}
-                           className="form-control"
-                           type="text"
-                           label="Количество наименований товаров" />
-                    <Field name="dispatcher"
-                           component={this.renderField}
-                           className="form-control"
-                           type="text"
-                           label="Диспетчер склада" />
-                    <Field name="registration-datetime"
-                           component={this.renderField}
-                           className="form-control"
-                           type="text"
-                           label="Дата и время регистрации накладной" />
-                    <label className="control-label">Описание товаров</label>
-                    <BootstrapTable data={
+                    <div className="form-group">
+                        <label className="control-label">Номер накладной</label>
+                        <input
+                            type="text"
+                            name="waybill-number"
+                            className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label className="control-label">Дата выписки накладной</label>
+                        <Calendar format='DD/MM/YYYY' />
+                    </div>
+                    <div className="form-group">
+                        <label className="control-label">Отправитель</label>
+                        <input
+                            type="text"
+                            name="sender"
+                            className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label className="control-label">Перевозчик</label>
+                        <input
+                            type="text"
+                            name="carrier"
+                            className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label className="control-label">Номера транспортных средств</label>
+                        <BootstrapTable data={[]} hover={true} striped={true}>
+                            <TableHeaderColumn dataField="type">Транспортное средство</TableHeaderColumn>
+                            <TableHeaderColumn isKey dataField="number">Номер</TableHeaderColumn>
+                        </BootstrapTable>
+                    </div>
+                    <div className="form-group">
+                        <label className="control-label">Водитель</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="driver" />
+                    </div>
+                    <div className="form-group">
+                        <label className="control-label">Дополнительное описание товарной партии</label>
+                        <textarea
+                            className="form-control"
+                            name="description" />
+                    </div>
+                    <div className="form-group">
+                        <label className="control-label">Сумма товаров по накладной</label>
+                        <input
+                                type="text"
+                                className="form-control"
+                                name="goods-total-sum"
+                                readonly="readonly" />
+                    </div>
+                    <div className="form-group">
+                        <label className="control-label">Количество товаров по накладной</label>
+                        <input
+                                type="text"
+                                className="form-control"
+                                name="goods-total-amount"
+                                readonly="readonly" />
+                    </div>
+                    <div className="form-group">
+                        <label className="control-label">Диспетчер склада</label>
+                        <input
+                                type="text"
+                                className="form-control"
+                                name="dispatcher"
+                                readonly="readonly" />
+                    </div>
+                    <div className="form-group">
+                        <label className="control-label">Дата и время регистрации накладной</label>
+                        <input
+                                type="text"
+                                className="form-control"
+                                name="goods-total-sum"
+                                readonly="readonly" />
+                    </div>
+                    <div className="form-group">
+                        <label className="control-label">Описание товаров</label>
+                        <BootstrapTable data={
                         [
                             {
                                 name: 'TESTNAME',
@@ -105,17 +111,16 @@ class WaybillRegistration extends React.Component {
                             }
                         ]
                     } hover={true} striped={false} >
-                        <TableHeaderColumn isKey dataField="name">Наименование</TableHeaderColumn>
-                        <TableHeaderColumn dataField="weight">Вес</TableHeaderColumn>
-                        <TableHeaderColumn dataField="price">Стоимость</TableHeaderColumn>
-                        <TableHeaderColumn dataField="storeWay">Способ хранения</TableHeaderColumn>
-                    </BootstrapTable>
+                            <TableHeaderColumn isKey dataField="name">Наименование</TableHeaderColumn>
+                            <TableHeaderColumn dataField="weight">Вес</TableHeaderColumn>
+                            <TableHeaderColumn dataField="price">Стоимость</TableHeaderColumn>
+                            <TableHeaderColumn dataField="storeWay">Способ хранения</TableHeaderColumn>
+                        </BootstrapTable>
+                    </div>
                 </form>
             </div>
         )
     }
 }
 
-export default connect(null, Actions)(reduxForm({
-    form: 'waybillRegistration'
-})(WaybillRegistration));
+export default connect(null, Actions)(WaybillRegistration);
