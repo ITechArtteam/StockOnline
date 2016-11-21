b0c5d0c9190532343af1caad7012fe18efa03d7e
speane <avtolubitel225@gmail.com>
2016-11-21 23:15:25 +0300

    Added custom component for a list of numbers

---
+++ src/main/frontend/src/reducers/waybillRegitrationForm.js
@@ -0,0 +1,18 @@
+const initialState = {
+    number: null,
+    registrationDate: null,
+    senderName: "",
+    senderId: null,
+    carrierName: "",
+    carrierId: null,
+    numbers: [],
+    driverId: null,
+    description: "",
+    products: [],
+    chooseSenderModalIsOpen: false,
+    chooseCarrierModalIsOpen: false
+};
+
+export default function waybillRegistrationForm(state = initialState, action) {
+
+}
