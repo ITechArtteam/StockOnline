064d6e7e8454573c0289a8ecf7012fa8e8094e8f
Natashkinsasha <natashkinsasha@gmail.com>
2016-11-21 06:51:47 +0300

    Добавил логирование в WorkerController

--- src/main/java/com/itechart/stockOnline/controller/WorkerController.java
+++ src/main/java/com/itechart/stockOnline/controller/WorkerController.java
@@ -2,6 +2,8 @@ package com.itechart.stockOnline.controller;
 
 import com.itechart.stockOnline.model.User;
 import com.itechart.stockOnline.service.WorkerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -11,31 +13,37 @@ import java.util.List;
 @RestController
 @RequestMapping(value="/api" )
 public class WorkerController {
+    private final static Logger LOGGER = LoggerFactory.getLogger(WorkerController.class);
     @Autowired
     private WorkerService workerService;
     @RequestMapping(value="/workers", method = RequestMethod.GET )
     public List<User> getWorkers(){
+        LOGGER.debug("REST request. Path:/workers  method: GET");
         return workerService.getAll();
     }
 
     @RequestMapping(value="/worker/{id}", method = RequestMethod.GET )
     public User getWorker(@PathVariable Long id){
+        LOGGER.debug("REST request. Path:/worker/{id}  method: GET", id);
         return workerService.get(id);
     }
 
     @RequestMapping(value="/worker/{id}", method = RequestMethod.DELETE )
     public void deleteWorker(@PathVariable Long id){
+        LOGGER.debug("REST request. Path:/worker/{id}  method: DELETE", id);
         workerService.delete(id);
     }
 
     @RequestMapping(value="/workers", method = RequestMethod.DELETE )
-    public void deleteWorkers(@RequestBody User[] users){
-        workerService.delete(users);
+    public void deleteWorkers(@RequestBody User[] workers){
+        LOGGER.debug("REST request. Path:/workers  method: DELETE Request body {workers}", workers);
+        workerService.delete(workers);
     }
 
     @RequestMapping(value="/worker", method = RequestMethod.POST)
-    public User saveWorker(@RequestBody User user){
-        return workerService.save(user);
+    public User saveWorker(@RequestBody User worker){
+        LOGGER.debug("REST request. Path:/worker  method: POST Request body {worker}", worker);
+        return workerService.save(worker);
     }
 
 
