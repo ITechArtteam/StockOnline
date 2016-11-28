cd7f1939d1d4834ec572d867c4bd2112dadacac9
SerhGitHub <serhiorsv@gmail.com>
2016-11-28 18:26:34 +0300

    Add the number of records after a search

--- src/main/frontend/src/containers/stocks/Stocks.js
+++ src/main/frontend/src/containers/stocks/Stocks.js
@@ -47,7 +47,7 @@ class Stocks extends React.Component {
     }
 
     onBtnDeleteClick() {
-        var selectedRowKeys = this.refs.table.state.selectedRowKeys;
+        let selectedRowKeys = this.refs.table.state.selectedRowKeys;
         if(selectedRowKeys.length == 0) {
             this.props.showDialog("Не выделена ни одна строка для удаления", []);
         } else {
@@ -91,57 +91,77 @@ class Stocks extends React.Component {
         bgColor: "rgb(238, 193, 213)"
     };
 
+    toEmptyStringIfNull = element => {
+        return element || '';
+    };
+
+    addPrefixIfNotEmpty = (element, prefix) => {
+        return (element.length === 0 ? '' : prefix) + element
+    };
+
     render() {
-        var stockList = this.props.page.stockList.map((item, index) => {
-            return {
+        let stockList = this.props.page.stockList.map((item, index) => {
+        let country = this.toEmptyStringIfNull(item.country);
+        let city = this.addPrefixIfNotEmpty(this.toEmptyStringIfNull(item.city), ' г. ');
+        let street = ' ' + this.toEmptyStringIfNull(item.street);
+        let home = this.addPrefixIfNotEmpty(this.toEmptyStringIfNull(item.home), ' д. ');
+        return {
                 rowNumber: ((this.props.page.activePage - 1) * this.props.page.itemsCountPerPage) + index + 1,
                 id: item.id,
                 name: item.name,
                 company: item.nameCompany,
-                address: item.country + ' г. ' + item.city + ' ул. ' + item.street + ' д. ' + item.home + ' кв. ' + item.room
+                address: country + city + street + home
                 }
                 });
 
         return (
-            <div className="container">
                 <div className="row">
                     <div className="col-xs-3">
-                        <div className="well well-sm">
-                            <button className="btn btn-default btn-block" onClick={this.onBtnSaveClick}>Создать</button>
-                            <button className="btn btn-default btn-block" onClick={this.onBtnDeleteClick}>Удалить</button>
-                        </div>
-                        <div className="well well-sm">
-                            <SimpleInput id="filterStockNameValue"
-                                         label="Склад"
-                                         value={this.props.frontend.filterStockNameValue}
-                                         onChange={this.onInputValueChane} />
-                            <SimpleInput id="filterAddressValue"
-                                         label="Адрес"
-                                         value={this.props.frontend.filterAddressValue}
-                                         onChange={this.onInputValueChane} />
-                            <button className="btn btn-default btn-block" onClick={this.onBtnSearchClick}>Поиск</button>
-                            <button className="btn btn-default btn-block" onClick={this.onBtnClearFilterClick}>Очистить фильтр</button>
-                        </div>
-                        <div className="well well-sm">
-                            <div className="form-inline">
-                                <div className="form-group">
-                                    <label htmlFor="pageLimitSelect">Записей на странице:</label>
+                        <div className="panel panel-default">
+                            <div className="panel-heading">Количество записей на странице</div>
+                                <div className="panel-body">
                                     <select className="form-control"
                                             ref={'pageLimitSelect'}
                                             id="pageLimitSelect"
                                             onChange={this.onPageLimitSelectChange}
-                                            defaultValue={this.props.page.itemsCountPerPage}
-                                    >
+                                            defaultValue={this.props.page.itemsCountPerPage}>
                                         <option>5</option>
                                         <option>10</option>
                                     </select>
                                 </div>
                             </div>
-                        </div>
-                    </div> {/*dib.col-xs-3 end*/}
+
+                        <div className="panel panel-default">
+                            <div className="panel-heading">Действия</div>
+                                <div className="panel-body">
+                                    <button className="btn btn-default btn-block" onClick={this.onBtnSaveClick}>Создать</button>
+                                    <button className="btn btn-default btn-block" onClick={this.onBtnDeleteClick}>Удалить</button>
+                                </div>
+                            </div>
+                        <div className="panel panel-default">
+                            <div className="panel-heading">Поиск складов</div>
+                                <div className="panel-body">
+                                    <SimpleInput id="filterStockNameValue"
+                                                 label="Склад"
+                                                 value={this.props.frontend.filterStockNameValue}
+                                                 onChange={this.onInputValueChange} />
+                                    <SimpleInput id="filterAddressValue"
+                                                 label="Адрес"
+                                                 value={this.props.frontend.filterAddressValue}
+                                                 onChange={this.onInputValueChange} />
+                                    <button className="btn btn-default btn-block" onClick={this.onBtnSearchClick}>Поиск</button>
+                                    <button className="btn btn-default btn-block" onClick={this.onBtnClearFilterClick}>Очистить фильтр</button>
+                                </div>
+                            </div>
+                        </div> {/*dib.col-xs-3 end*/}
                     <div className="col-xs-9">
+                        <div className={this.props.frontend.isFilterMessageVisible ? 'before-table' : 'none'}>
+                            <span className="label label-success">
+                            По вашему запросу найдено {this.props.page.totalItemsCount} записей
+                            </span>
+                        </div>
                         <BootstrapTable data={stockList} selectRow={this.selectRowProp} striped={true} hover={true} ref="table">
-                            <TableHeaderColumn headerAlign="center" dataField="id" isKey={true} dataFormat={this.nameFormatter}>Номер склада</TableHeaderColumn>
+                            <TableHeaderColumn headerAlign="center" dataField="id" width="60" isKey={true}  dataFormat={this.nameFormatter} >Номер склада</TableHeaderColumn>
                             <TableHeaderColumn headerAlign="center" dataField="name" >Склад</TableHeaderColumn>
                             <TableHeaderColumn headerAlign="center" dataField="address">Адрес</TableHeaderColumn>
                             <TableHeaderColumn headerAlign="center" dataField="company">Название компании</TableHeaderColumn>
@@ -161,7 +181,6 @@ class Stocks extends React.Component {
                         type={this.props.alert.type}
                     />
                 </div>{/*div.row end*/}
-            </div>/*div.container end*/
         )
     }
 }
@@ -180,8 +199,8 @@ class Stocks extends React.Component {
             getStockList: (pageNumber, itemsCountPerPage) => {
             dispatch(stockListActionCreator.getStockList(pageNumber, itemsCountPerPage))
             },
-            showDialog: (text, buttons) => {
-                dispatch(stockListActionCreator.showDialog(text, buttons))
+            showDialog: (text, type, buttons) => {
+                dispatch(stockListActionCreator.showDialog(text, type, buttons))
             },
 
             closeDialog: () => {
