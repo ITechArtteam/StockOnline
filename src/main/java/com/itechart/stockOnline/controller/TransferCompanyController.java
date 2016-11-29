a3642b2b077757d1863987bc0e0e6711c87eac0d
speane <avtolubitel225@gmail.com>
2016-11-29 03:31:53 +0300

    Created controller for retrieving info about transfer companies

---
+++ src/main/java/com/itechart/stockOnline/controller/TransferCompanyController.java
@@ -0,0 +1,32 @@
+package com.itechart.stockOnline.controller;
+
+import com.itechart.stockOnline.model.TransferCompany;
+import com.itechart.stockOnline.model.dto.transfercompany.TransferCompanyDto;
+import com.itechart.stockOnline.model.dto.transfercompany.TransferCompanyDtoBuilder;
+import com.itechart.stockOnline.service.transfercompany.TransferCompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/transfercompanies")
+public class TransferCompanyController {
+
+    @Autowired
+    private TransferCompanyService transferCompanyService;
+
+    @Autowired
+    private TransferCompanyDtoBuilder dtoBuilder;
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public List<TransferCompanyDto> getAllTransferCompanies() {
+        List<TransferCompany> transferCompanies = transferCompanyService.getAll();
+
+        return dtoBuilder.createDtoList(transferCompanies);
+    }
+}
