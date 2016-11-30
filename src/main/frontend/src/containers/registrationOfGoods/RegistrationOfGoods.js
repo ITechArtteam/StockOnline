7f42a2ee1bae1ec17f68593fd83f4bd75f690089
orelDmitriy <oreldmitriy1991@gmail.com>
2016-11-30 10:41:37 +0300

    set style for registrationOfGoods

--- src/main/frontend/src/containers/registrationOfGoods/RegistrationOfGoods.js
+++ src/main/frontend/src/containers/registrationOfGoods/RegistrationOfGoods.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 import {Link} from "react-router";
 
 class RegistrationOfGoods extends React.Component {
     render() {
         return (
             <div className="row">
-                <Link to="/registrationOfGoods/driver">Автомобиль</Link>
-                <Link to="/registrationOfGoods/train">Поезд</Link>
+                <div
+                    className="well well-sm col-sm-5 col-md-5 col-lg-5 col-sm-offset-3 col-md-offset-3 col-lg-offset-3">
+                    <div className="btn-group function-button-group choseButtonGroup" role="group">
+                        <Link to="/registrationOfGoods/driver" className="btn btn-default">Автомобиль</Link>
+                        <Link to="/registrationOfGoods/train" className="btn btn-default">Поезд</Link>
+                    </div>
+                </div>
             </div>
         )
     }
