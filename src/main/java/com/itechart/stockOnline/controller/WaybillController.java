7fbba1d8d2738966893d78d207a516972e22c48a
speane <avtolubitel225@gmail.com>
2016-12-05 02:19:10 +0300

    Added exception handling to waybill controller

--- src/main/java/com/itechart/stockOnline/controller/WaybillController.java
+++ src/main/java/com/itechart/stockOnline/controller/WaybillController.java
@@ -5,7 +5,10 @@ import com.itechart.stockOnline.model.dto.waybillregistration.WaybillBuilder;
 import com.itechart.stockOnline.model.dto.waybillregistration.WaybillRegistrationDto;
 import com.itechart.stockOnline.model.enums.WaybillStatus;
 import com.itechart.stockOnline.service.WaybillService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
@@ -13,6 +16,8 @@ import java.text.ParseException;
 @RestController
 @RequestMapping(value = "/waybills")
 public class WaybillController {
+
+    private static final Logger logger = LoggerFactory.getLogger(WaybillController.class);
 
     @Autowired
     private WaybillBuilder waybillBuilder;
@@ -33,5 +38,11 @@ public class WaybillController {
         waybill.setStatus(WaybillStatus.JOINED);
 
         waybillService.save(waybill);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void handleException(Exception exception) {
+        logger.error("Error occurred: {}", exception.getMessage());
     }
 }
