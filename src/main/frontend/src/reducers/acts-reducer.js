0a253ddd584bd2b20f00948690ebb5ece1b06c9c
Natashkinsasha <natashkinsasha@gmail.com>
2016-12-11 02:01:36 +0300

    переправил acts-reducer

--- src/main/frontend/src/reducers/acts-reducer.js
+++ src/main/frontend/src/reducers/acts-reducer.js
@@ -4,34 +4,17 @@ import _ from "lodash";
 const initialState = {
         response: null,
         acts: [],
-        act: {
-            id: "",
-            reportDate: "",
-            products_in_act:[],
-            user:{
-                id:"",
-                login:""
-            }
-        }
-
     }
     ;
-const initialAct ={
-    id: "",
-    reportDate: "",
-    products_in_act:[],
-    user:{
-        id:"",
-        login:""
-    }
-}
 
 const actsReducer = function (state = initialState, action) {
     switch (action.type) {
         case types.GET_ACTS_SUCCESS:
             return Object.assign({}, state, {acts: action.acts});
 
-
+        case types.DELETE_ACT_SUCCESS:
+            var newActs = _.filter(state.acts, acts => act.id != action.id);
+            return Object.assign({}, state, {acts: newActs, response: action.response});
 
         case types.GET_ACTS_UNSUCCESS:
             return Object.assign({}, state, {response: action.response});
@@ -44,37 +27,17 @@ const actsReducer = function (state = initialState, action) {
             return Object.assign({}, state, {acts: newActs, response: action.response});
 
         case types.POST_ACT_SUCCESS:
-            var newActs = _.concat(state.acts, action.act);
-            return Object.assign({}, state, {acts: newActs, response: action.response});
-
-        case types.DELETE_ACT_SUCCESS:
-            var newActs = _.filter(state.acts, acts => act.id != action.id);
-            return Object.assign({}, state, {acts: newActs, response: action.response});
+            return Object.assign({}, state, {response: action.response});
 
         case types.CLOSE_ACTS_RESPONSE:
             return Object.assign({}, state, {response: null});
 
+
         case types.INITIAL_STATE_ACTS:
             return Object.assign({}, state, initialState);
-
-
-
-        case types.GET_ACT_SUCCESS:
-            return Object.assign({}, state, {act: action.act});
-
-        case types.GET_ACT_UNSUCCESS:
-            return Object.assign({}, state, {response: action.response});
-
-        case types.DELETE_ACT_UNSUCCESS:
-            return Object.assign({}, state, {response: action.response});
-
-        case types.POST_ACT_UNSUCCESS:
-            return Object.assign({}, state, {response: action.response, act: action.act});
-
-        case types.INITIAL_STATE_ACT:
-            return Object.assign({}, state, {act:initialAct});
     }
     return state;
 }
 
 export default actsReducer;
+
