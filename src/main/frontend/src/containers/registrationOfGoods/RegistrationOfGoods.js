ba37c092127cce7c659748d58ad39f928340e152
SerhGitHub <serhiorsv@gmail.com>
2016-12-15 00:39:02 +0300

    Добавил надпись 'Вид транспорта' при регистрации товара

--- src/main/frontend/src/containers/registrationOfGoods/RegistrationOfGoods.js
+++ src/main/frontend/src/containers/registrationOfGoods/RegistrationOfGoods.js
@@ -8,7 +8,8 @@ class RegistrationOfGoods extends React.Component {
             <div className="row">
                 <div
                     className="well well-sm col-sm-5 col-md-5 col-lg-5 col-sm-offset-3 col-md-offset-3 col-lg-offset-3">
-                    <div className="btn-group function-button-group choseButtonGroup" role="group">
+                        <div className="panel-heading">Вид транспорта</div>
+                        <div className="btn-group function-button-group choseButtonGroup" role="group">
                         <Link to="/registrationOfGoods/driver" className="btn btn-default">Автомобиль</Link>
                         <Link to="/registrationOfGoods/train" className="btn btn-default">Поезд</Link>
                     </div>
